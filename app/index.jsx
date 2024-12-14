9a708750e6408f025922cbc3c8fe52c8233dba44
Arshad <arshadchaudhary9819@gmail.com>
2024-12-14 21:20:13 +0530

    Measure

---
+++ app/index.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function App(){
+    return(
+        <View className="bg-black/80">
+            <Text className="text-white text-2xl">What's up Jenix</Text>
+        </View>
+    )
+}
