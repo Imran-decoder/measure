1b6db62c97f73221db4eeb77b9732b9aaed6d9f6
imran <imran.221404.it@mhssce.ac.in>
2024-12-14 21:31:37 +0530

    updated

--- app/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function App(){
-    return(
-        <View className="bg-black/80">
-            <Text className="text-white text-2xl">What's up Jenix</Text>
-        </View>
-    )
-}
